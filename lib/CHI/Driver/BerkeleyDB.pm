789c9deb6e4fadaf5cc282c8c86b3771c190b60a
Jonathan Swartz <swartz@pobox.com>
2009-06-23 05:55:09 -0400

    remove accidental .svn files

--- lib/CHI/Driver/BerkeleyDB.pm
+++ lib/CHI/Driver/BerkeleyDB.pm
@@ -11,40 +11,50 @@ extends 'CHI::Driver';
 
 our $VERSION = '0.01';
 
-has 'db'   => ( is => 'ro', init_arg => undef, lazy_build => 1 );
-has 'file' => ( is => 'ro', init_arg => undef, lazy_build => 1 );
-has 'mgr'  => ( is => 'ro', init_arg => undef, lazy_build => 1 );
+has 'db'       => ( is => 'ro', init_arg => undef, lazy_build => 1 );
+has 'filename' => ( is => 'ro', init_arg => undef, lazy_build => 1 );
+has 'mgr'      => ( is => 'ro', init_arg => undef, lazy_build => 1 );
 has 'root_dir' => ( is => 'ro', required => 1 );
 
 __PACKAGE__->meta->make_immutable();
+
+sub BUILD {
+    my ( $self, $params ) = @_;
+
+    $self->{mgr_params} = $self->non_common_constructor_params($params);
+}
 
 sub _build_mgr {
     my $self = shift;
     return BerkeleyDB::Manager->new(
         home   => $self->root_dir,
-        create => 1
+        create => 1,
+        %{ $self->{mgr_params} },
     );
 }
 
-sub _build_file {
+sub _build_filename {
     my $self = shift;
     return $self->escape_for_filename( $self->namespace ) . ".db";
 }
 
 sub _build_db {
     my $self = shift;
-    return $self->mgr->open_db( file => $self->file );
+
+    my $filename = $self->filename;
+    for (1..3) {
+        if (my $db = eval { $self->mgr->open_db( file => $filename ) }) {
+            return $db;
+        }
+    }
+    die sprintf("cannot open '%s/%s': %s %s", $self->root_dir, $filename, $!, $@);
 }
 
 sub fetch {
     my ( $self, $key ) = @_;
 
-    if ( $self->db->db_get( $key, my $data ) == 0 ) {
-        return $data;
-    }
-    else {
-        return undef;
-    }
+    my $data;
+    return ( $self->db->db_get( $key, $data ) == 0 ) ? $data : undef;
 }
 
 sub store {
@@ -64,11 +74,9 @@ sub remove {
 sub clear {
     my ($self) = @_;
 
-    undef $self->{db};
-    undef $self->{mgr};
-    unlink( $self->root_dir . "/" . $self->file );
-    $self->{mgr} = $self->_build_mgr;
-    $self->{db}  = $self->_build_db;
+    delete( $self->{db} );
+    delete( $self->{mgr} );
+    unlink( $self->root_dir . "/" . $self->filename );
 }
 
 sub get_keys {
@@ -86,8 +94,7 @@ sub get_keys {
 sub get_namespaces {
     my ($self) = @_;
 
-    my $root_dir = $self->root_dir;
-    my @contents = read_dir($root_dir);
+    my @contents = read_dir( $self->root_dir );
     my @namespaces =
       map { $self->unescape_for_filename( substr( $_, 0, -3 ) ) }
       grep { /\.db$/ } @contents;
@@ -106,11 +113,31 @@ CHI::Driver::BerkeleyDB -- Using BerkeleyDB for cache
 
 =head1 SYNOPSIS
 
-    use CHI::Driver::BerkeleyDB;
+    use CHI;
+
+    my $cache = CHI->new(
+        driver     => 'BerkeleyDB',
+        root_dir   => '/path/to/cache/root'
+    );
 
 =head1 DESCRIPTION
 
-CHI::Driver::BerkeleyDB provides
+This cache driver uses Berkeley DB files to store data. Each namespace is stored in its own db file.
+
+=head1 CONSTRUCTOR OPTIONS
+
+=over
+
+=item root_dir
+
+Path to the directory that will contain the database files, also known as the
+BerkeleyDB "Home".
+
+=back
+
+Any other constructor options L<not recognized by CHI|CHI/constructor> are
+passed along to L<BerkeleyDB::Manager-E<gt>new>. For example, you can pass
+I<db_class> to change from the default BerkeleyDB::Hash.
 
 =head1 AUTHOR
 
@@ -118,7 +145,7 @@ Jonathan Swartz
 
 =head1 SEE ALSO
 
-L<CHI>
+L<CHI>, L<BerkeleyDB>, L<BerkeleyDB::Manager>
 
 =head1 COPYRIGHT & LICENSE
 
@@ -129,10 +156,6 @@ warranties, including, without limitation, the implied warranties of
 merchantibility and fitness for a particular purpose.
 
 This program is free software; you can redistribute it and/or modify it under
-the same terms as Per
-
-l it
-
-self.
+the same terms as Perl itself.
 
 =cut
--- lib/CHI/Driver/BerkeleyDB/t/CHIDriverTests.pm
+++ lib/CHI/Driver/BerkeleyDB/t/CHIDriverTests.pm
@@ -2,10 +2,12 @@ package CHI::Driver::BerkeleyDB::t::CHIDriverTests;
 use strict;
 use warnings;
 use CHI::Test;
+use CHI::Driver::BerkeleyDB::Util;
+use File::Slurp;
 use File::Temp qw(tempdir);
 use base qw(CHI::t::Driver);
 
-my $root_dir;
+my ($root_dir, $root_dir_initialized);
 
 sub testing_driver_class { 'CHI::Driver::BerkeleyDB' }
 
@@ -13,11 +15,20 @@ sub required_modules {
     return { 'BerkeleyDB::Manager' => undef, 'BerkeleyDB' => undef };
 }
 
+sub clear_root_dir : Test(setup) {
+    $root_dir_initialized = 0;
+}
+
 sub new_cache_options {
     my $self = shift;
 
-    $root_dir ||=
-      tempdir( "chi-driver-berkeleydb-XXXX", TMPDIR => 1, CLEANUP => 1 );
+    # Generate new temp dir for each test method that needs it;
+    # previous temp dir gets cleaned up immediately
+    #
+    if ( !($root_dir_initialized++) ) {
+        $root_dir =
+            File::Temp->newdir( "chi-driver-berkeleydb-XXXX", TMPDIR => 1 );
+    }
     return ( $self->SUPER::new_cache_options(), root_dir => $root_dir );
 }
 
