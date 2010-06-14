fab522201f3f5b4a03f6ba71da3101e61cf3d468
Jonathan Swartz <swartz@pobox.com>
2010-06-14 15:59:26 -0700

    simplify, make like other driver tests

--- lib/CHI/Driver/BerkeleyDB/t/CHIDriverTests.pm
+++ lib/CHI/Driver/BerkeleyDB/t/CHIDriverTests.pm
@@ -6,25 +6,17 @@ use File::Slurp;
 use File::Temp qw(tempdir);
 use base qw(CHI::t::Driver);
 
-my ( $root_dir, $root_dir_initialized );
+my $root_dir;
 
 sub testing_driver_class { 'CHI::Driver::BerkeleyDB' }
-
-sub clear_root_dir : Test(setup) {
-    $root_dir_initialized = 0;
-}
 
 sub new_cache_options {
     my $self = shift;
 
-    # Generate new temp dir for each test method that needs it;
-    # previous temp dir gets cleaned up immediately
-    #
-    if ( !( $root_dir_initialized++ ) ) {
-        $root_dir =
-          File::Temp->newdir( "chi-driver-berkeleydb-XXXX", TMPDIR => 1 );
-    }
-    return ( $self->SUPER::new_cache_options(), root_dir => $root_dir );
+    $root_dir ||=
+      tempdir( "chi-driver-berkeleydb-XXXX", TMPDIR => 1, CLEANUP => 1 );
+    return ( $self->SUPER::new_cache_options(),
+        root_dir => "$root_dir/berkeleydb" );
 }
 
 1;
